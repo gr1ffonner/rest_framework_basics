5126ea35dd89473cfeac12bc19353cdf33cec561
Danya <danya.rodnyakov228@gmail.com>
2023-05-16 21:40:16 +0300

    add models

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ("manager", "Manager"),
+        ("employee", "Employee"),
+    )
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+    name = models.CharField(max_length=50)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    assigned_to = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="tasks"
+    )
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Message(models.Model):
+    content = models.TextField()
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
