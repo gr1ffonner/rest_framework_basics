45fc67a9335fd8e6cb1f79d5ec8a618c857004b3
Danya <danya.rodnyakov228@gmail.com>
2023-05-21 10:03:40 +0300

    email auth

--- config/settings.py
+++ config/settings.py
@@ -141,11 +141,7 @@ REST_AUTH_REGISTER_SERIALIZERS = {
 }
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
-AUTHENTICATION_BACKENDS = [
-    # allauth specific authentication methods, such as login by e-mail
-    "allauth.account.auth_backends.AuthenticationBackend",
-    # Needed to login by username in Django admin, regardless of allauth
-    "django.contrib.auth.backends.ModelBackend",
-]
+ACCOUNT_EMAIL_VERIFICATION = "mandatory"
+ACCOUNT_EMAIL_REQUIRED = True
 
 SITE_ID = 1
--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.urlpatterns import re_path
 from tasks.views import CustomRegisterView
+from dj_rest_auth.registration.views import VerifyEmailView
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -11,5 +14,15 @@ urlpatterns = [
         "api/v1/dj-rest-auth/registration/",
         CustomRegisterView.as_view(),
         name="rest_register",
+    ),
+    re_path(
+        r"^account-confirm-email/",
+        VerifyEmailView.as_view(),
+        name="account_email_verification_sent",
+    ),
+    re_path(
+        r"^account-confirm-email/(?P<key>[-:\w]+)/$",
+        VerifyEmailView.as_view(),
+        name="account_confirm_email",
     ),
 ]
